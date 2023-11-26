7adde7f90c6d8f0026ba402650a469d4aee2d5cc
Shubh Goyal <goyal.shubh0711@gmail.com>
2023-11-26 05:57:53 +0530

    Relevant project extraction pipeline added
    The pipeline to extract relevant projects from all the projects of a candidate given a job description added.
    
    Finetuning of the pipeline is needed.

---
+++ pro_jdk.py
@@ -0,0 +1,99 @@
+import os
+import openai
+
+# Load .env file
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+# Importing modules for langchain
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from langchain.utils.openai_functions import convert_pydantic_to_openai_function
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.output_parsers.openai_functions import JsonOutputFunctionsParser, JsonKeyOutputFunctionsParser
+
+
+# Pydantic models
+# Project model
+class Project(BaseModel):
+    """A project with a name and description."""
+
+    name: str = Field(description="The name of the project.")
+    # description: str = Field(description="A description of the project.")
+    relevance: float = Field(description="The relevance of the project to the job description.")
+    reason: bool = Field(description="relation of project, true if directly relevant, false otherwise")
+
+# Experience model
+class Experience(BaseModel):
+    """Projects that a candidate has worked on."""
+
+    projects: List[Project] = Field(
+        description="A list of projects that the candidate has worked on."
+    )
+
+
+# OpenAI model
+model = ChatOpenAI(model='gpt-3.5-turbo' ,temperature=0)
+
+
+# Convert Pydantic model to OpenAI function
+extraction_functions = [convert_pydantic_to_openai_function(Experience)]
+extraction_model = model.bind(functions=extraction_functions, function_call={"name": "Experience"})
+
+
+# Template
+template = """A job description will be passed to you along with a candidate's project experience.
+
+You will be asked to extract the projects relevant to the job description.
+
+If no project mentioned are relevant it's fine - you don't need to extract any! Just return an empty list.
+
+Do not make up any new project. Strictly return the relevant projects only.
+
+The company's jdk is as follows:"""
+
+
+# Company's job description
+# jdk = """Data analysis and applicability study of algorithm using AI technologies such as machine learning, deep learning, etc. Study and evaluate AI algorithm and NTT Laboratories research products including log analysis AI of telecommunication network, image analysis AI, and so on. For example, trainee is expected to work on the following items:  ++ AI implementation (using machine learning, deep learning, etc.) with Python ++ Study/evaluation of certain algorithms' applicability ++ Analysis and visualization of those results (MS Excel, BI tool)"""
+jdk = """Join our dynamic team as a Web Development Intern. In this role, you'll work closely with our experienced developers to contribute to the design, coding, testing, and deployment of web applications. This internship offers hands-on experience with HTML, CSS, and JavaScript, as well as exposure to popular web development frameworks."""
+
+# Chat prompt
+prompt = ChatPromptTemplate.from_messages([
+    ("system", template + "\n" + jdk),
+    ("user", "What projects are relevant to the company's requirements?"),
+    ("human", "{input}")
+])
+
+
+# Chain
+# extraction_chain = prompt | extraction_model | JsonOutputFunctionsParser()
+# extraction_chain = prompt | extraction_model
+extraction_chain = prompt | extraction_model | JsonKeyOutputFunctionsParser(key_name="projects")
+
+
+# Project experience
+project_prompt = """I have worked on the following projects:
+
+1. Literature Society IITJ Website:
+    A website developed for managing various activities for literature society of IIT Jodhpur. The website contains the following features:
+        -User Profile Management (with in site notification service)
+        -Library Management System
+        -Feature to upload written works to be displayed on website (Reader's Section).
+        -Moderator and Admin access profiles to approve books and contents.
+
+2. Multi Model Data Analysis for Annotation of Human Activities:
+    -Performed data analysis and outlier detection on a dataset of accelerometer and gyroscope readings.
+    -Ideated and executed a pipeline for performing cleaning and detecting the activity type throught the dataset.
+
+3. Cloudphysician's Vital Extraction Challenge:
+    -Fine-tuned DETR and detectron to for ICU monitor segmentation images of ICU rooms and vital segmentation from ICU monitors.
+    -Worked on graph digitisation algorithm to digitize ECG strip.
+"""
+
+
+# Response
+response = extraction_chain.invoke({"input": project_prompt})
+
+print(response)
